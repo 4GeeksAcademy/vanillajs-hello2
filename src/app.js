3f7d693d6ccfcd27ff815ed14c450ee43a39807f
Luis <166702635+LuisIannelloDev@users.noreply.github.com>
2024-05-29 19:23:46 +0000

    Resuelto

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function generarDominio() {
+    let dominio =
+      pronoun[Math.floor(Math.random() * pronoun.length)] +
+      adj[Math.floor(Math.random() * adj.length)] +
+      noun[Math.floor(Math.random() * noun.length)] +
+      ".com";
+    return dominio;
+  }
+  document.getElementById("dominio").innerHTML = generarDominio();
 };
